329187c400e621d254b8caafdb69eeb2281b9eb5
sushihi <tanmayjain3567@gmail.com>
2026-07-10 13:15:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function startGame(){
 
+let html=`
+
+<h2>Card 1 / 11</h2>
+
+<h1>🇮🇳 India 2010s</h1>
+
+<h3>Select a Player</h3>
+
+`;
+
+eras[0].players.forEach(function(player){
+
+html+=`
+
+<button class="player">
+
+${player}
+
+</button>
+
+`;
+
+});
+
+document.getElementById("app").innerHTML=html;
+
+}
